9e3ddf36be4204a22f62bbbf5e04ec13a8d94353
Reynaldo Endis <rendis@agilistik.cl>
2021-04-16 03:09:21 -0400

    Adding routing

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Layout from '../containers/Layout';
 import OrdersList from '../components/OrdersList';
+import SyncLayout from '../components/shipedge-sync/SyncLayout';
 
 const App = () => (
-    <Layout>
-        <OrdersList/>
-    </Layout>
+    <BrowserRouter>
+        <Layout>
+            <Switch>
+                <Route exact path="/" component={SyncLayout}/>
+                <Route exact path="/orders" component={OrdersList}/>
+            </Switch>
+        </Layout>
+    </BrowserRouter>
 );
 
 export default App;
