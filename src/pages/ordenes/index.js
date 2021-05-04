85d31d185ae1b92c36af5950c189e4f3cd77e33c
Gustavo Rivero <magalaz@stefanini.com>
2021-05-03 21:57:27 -0400

    Order Refresh success/error components whit styles

--- src/pages/ordenes/index.js
+++ src/pages/ordenes/index.js
@@ -9,21 +9,28 @@ import MainTable from 'components/Templates/MainTable';
 import OrderDetail from 'components/Molecules/OrderDetail';
 import PageTitle from 'components/Atoms/PageTitle';
 import reload from 'assets/brand/reload.svg';
+import warningIcon from 'assets/brand/warningIcon.svg';
+import styles from './styles.module.scss'
 
 const Orders = () => {
     const [loading, setLoading] = useState(true);
     const [modal, setModal] = useState(false);
     const [error, setError] = useState(false);
+    const [isUpdate, setIsUpdate] = useState(false);
+    const [date, setDate] = useState(null);
     const [list, setList] = useState([]);
+    const [message, setMessage] = useState('');
     const [totalPages, setTotalPages] = useState('');
     const [orderId, setOrderId] = useState('');
     const [orderNumber, setOrderNumber] = useState('');
     const [orderTracking, setOrderTracking] = useState('');
-    const [date, setDate] = useState(null);
+
 
     let history = useHistory();
 
     const getData = () => {
+        setError(false);
+        setMessage('');
         clientFetch('order/getOrderList', {
             body: {
                 "page": 1,
@@ -31,7 +38,7 @@ const Orders = () => {
                 "status": "all"
             }
         })
-            .then(data => {
+            .then((data) => {
                 // console.log('orderData:', data);
                 setLoading(false);
                 setList(data.order);
@@ -55,13 +62,16 @@ const Orders = () => {
                 setDate({
                     day: DATE.getDay(),
                     month: monthNames[DATE.getMonth()],
-                    time: `${DATE.getHours()} : ${DATE.getMinutes()}`,
-                })
+                    time: `${DATE.getHours()} : ${DATE.getMinutes() < 10 ? '0' : ''}${DATE.getMinutes()}`,
+                });
+                setMessage('success');
             })
-            .catch(error => {
+            .catch((error) => {
                 console.log('error', error);
                 setError(true);
                 setLoading(false);
+                setMessage('error');
+                setDate(null);
             });
     }
 
@@ -118,23 +128,41 @@ const Orders = () => {
     const handleClickUpdateList = (e) => {
         e.preventDefault();
         setLoading(true);
+        setIsUpdate(true);
         getData();
+        setTimeout(() => {
+            setIsUpdate(false)
+        }, 3000)
     }
 
     const handleClickOrderDeatil = (e, tableData) => {
         e.preventDefault();
         setOrderId(tableData.row.original.order_id);
         setOrderNumber(tableData.row.original.order_number);
-        setOrderTracking(tableData.row.original.tracking_description);
+        setOrderTracking(tableData.row.original.numero_tracking);
         setModal(true);
     }
     
     let component;
-
     if (error) {
-      component = <Alert className="mt-5" type="warning" text="Ooopss! Ocurrió un error, intentalo más tarde..."/>;
+        component = <Alert className="mt-5" type="warning" text="Ooopss! Ocurrió un error, intentalo más tarde..."/>;
     } else {
-      component = <Spinner />;
+        component = <Spinner />;
+    }
+    
+    let componentMessage;
+    switch (message) {
+        case 'success':
+            componentMessage = <p className="py-5"><span className={`${styles.badge}`}>¡Tus órdenes han sido actualizadas con éxito!</span></p>;
+            break;
+        case 'error':
+            componentMessage = <p className="py-5"><span className={`${styles.badge} ${styles.errorMessage}`}> 
+                <img src={warningIcon} alt="Actualizar Ordenes" width="25"/>
+                <span> Ups, ¡No se logro actualizar! </span>
+            </span></p>;
+            break;
+        default:
+            componentMessage = null;
     }
 
     useEffect(() => {
@@ -143,16 +171,19 @@ const Orders = () => {
     return (
         <>
             <PageTitle title="Tus órdenes" subtitle="Te mostramos tus órdenes de los últimos días"/>
-            {date && (
+            {isUpdate && (
+                componentMessage
+            )}
+            {date && !isUpdate && (
                 <a href="#!" onClick={handleClickUpdateList}>
                     <div className="pt-2 mb-5 d-flex align-items-center">
                         <span className="me-2">Última actualización</span>
-                        <span className="me-2">{`${date.day} ${date.month} ${date.time}`}</span>
+                        <span className="me-2">{`${date.day}, ${date.month} ${date.time}`}</span>
                         <img src={reload} alt="Actualizar Ordenes" width="19"/>
                     </div>
                 </a>
             )}
-            {list.length && !loading
+            {list.length && !loading && !error
                 ? <MainTable 
                     columns={columns}
                     data={data}
