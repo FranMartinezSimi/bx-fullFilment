77d015b6e7a232e216d73c313e820681964d494c
Gustavo Rivero <magalaz@stefanini.com>
2021-05-05 16:59:25 -0400

    Styles chenge typography

--- src/components/Atoms/PageTitle/index.js
+++ src/components/Atoms/PageTitle/index.js
@@ -3,9 +3,9 @@ import React from 'react'
 const PageTitle = ({title, subtitle, className}) => {
   return (
     <div className={className}>
-      <h1 className="display-font" style={{fontWeight: 900}}>{title}</h1>
+      <h1 className="display-font">{title}</h1>
       {subtitle && 
-        <h4 className="display-font" style={{fontWeight: 900, fontSize: "18px"}}>{subtitle}</h4>
+        <h4 className="display-font" style={{fontSize: "18px"}}>{subtitle}</h4>
       }
     </div>
   );
