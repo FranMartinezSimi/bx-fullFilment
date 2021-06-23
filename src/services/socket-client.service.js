56a9ceab8cce75b8513e80035b6d131d8ec8c8a9
necrocyber <amedel.zen@gmail.com>
2021-06-23 19:45:35 -0400

    add header socket

--- src/services/socket-client.service.js
+++ src/services/socket-client.service.js
@@ -1,5 +1,5 @@
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:9000');
+const socket = io('https://devapigw.bluex.cl/api/fulfillment/notifications/v1', { extraHeaders: { apikey: 'PDY4iyrXsHe16a8OTDl5OghRpJ25qSIt' } });
 
 export default socket;
