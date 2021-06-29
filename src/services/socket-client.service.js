056988faf1cf0843f32080301ac6112d6dd8e60a
necrocyber <amedel.zen@gmail.com>
2021-06-29 18:34:47 -0400

    add socket client 2.3

--- src/services/socket-client.service.js
+++ src/services/socket-client.service.js
@@ -3,6 +3,7 @@ import io from 'socket.io-client';
 // Llamada al socket
 const socket = io('https://devapigw.bluex.cl', {
   path: '/api/fulfillment/notifications/v1',
+  reconnectionAttemps: 0,
 });
 
 export default socket;
