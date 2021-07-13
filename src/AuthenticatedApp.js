cfc427f5be68c3b7fd0bc4d658ae85e34af5074c
Gustavo Rivero <tavomak@gmail.com>
2021-07-13 19:12:27 -0400

    Onboarding finished

--- src/AuthenticatedApp.js
+++ src/AuthenticatedApp.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import { useAuth } from 'context/userContex';
 
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router, Switch, Route,
+} from 'react-router-dom';
 import ErrorPage from 'pages/404';
 import Layout from './components/Templates/Layout';
 import Home from './pages/home';
@@ -12,34 +14,46 @@ import UpdateOrders from './pages/ordenes/subir-ordenes';
 
 import './styles/main.scss';
 
-const App = () => (
-  <Router>
-    <Layout>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/ordenes">
-          <Orders />
-        </Route>
-        <Route exact path="/onboarding">
-          <Onboarding />
-        </Route>
-        <Route exact path="/error-ordenes">
-          <Errors />
-        </Route>
-        <Route exact path="/inventario">
-          <Inventory />
-        </Route>
-        <Route exact path="/ordenes/subir-ordenes">
-          <UpdateOrders />
-        </Route>
-        <Route path="*">
-          <ErrorPage />
-        </Route>
-      </Switch>
-    </Layout>
-  </Router>
-);
+const App = () => {
+  const { user } = useAuth();
+  const userData = JSON.parse(user);
+  const userActive = userData.credential.user.onboarding
+    ? userData.credential.user.onboarding
+    : false;
+  return (
+    <Router>
+      <Layout>
+        <Switch>
+          {userActive
+            ? (
+              <Route exact path="/">
+                <Home />
+              </Route>
+            )
+            : (
+              <Route exact path="/">
+                <Onboarding />
+              </Route>
+            )}
+          <Route exact path="/ordenes">
+            <Orders />
+          </Route>
+          <Route exact path="/error-ordenes">
+            <Errors />
+          </Route>
+          <Route exact path="/inventario">
+            <Inventory />
+          </Route>
+          <Route exact path="/ordenes/subir-ordenes">
+            <UpdateOrders />
+          </Route>
+          <Route path="*">
+            <ErrorPage />
+          </Route>
+        </Switch>
+      </Layout>
+    </Router>
+  );
+};
 
 export default App;
--- src/components/Atoms/NavigationDots/index.js
+++ src/components/Atoms/NavigationDots/index.js
@@ -1,11 +1,18 @@
-const NavigationDots = ({ list }) => {
-  console.log(list);
+import styles from './styles.module.scss';
+
+const NavigationDots = ({ list, setListItem }) => {
+  const handleClick = (e, key) => {
+    e.preventDefault();
+    setListItem(e, key);
+  };
   return (
-    <ul>
+    <ul className="d-flex w-100 justify-content-center">
       {list !== undefined && list.length > 0 && (
-        list.map((item) => (
-          <li key={item.name}>
-            <a href="!#" className={item.active ? 'active' : ''}>{item.name}</a>
+        list.map((item, key) => (
+          <li key={item.name} className="mx-2">
+            <a href="!#" onClick={(e) => handleClick(e, key)}>
+              <span className={`${styles.dot} ${item.active ? styles.dotActive : ''}`} />
+            </a>
           </li>
         ))
       )}
